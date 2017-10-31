62469d25942e727b29be910af38f59103ae9a7ff
panda <panda@developmentontheedge.com>
2017-10-31 18:55:42 +0700

    +

--- src/com/company/SentenceArranger.java
+++ src/com/company/SentenceArranger.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import com.company.support.AnsiColor;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,9 +20,40 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
+import static com.company.support.AnsiColor.*;
+
 public class SentenceArranger {
 
-    private static String searchBreked = AnsiColor.RED + " Поиск прерван " + AnsiColor.RESET;
+
+    // Поля которые веротно нужно будет настроить:
+
+    private static Integer WIKI_FILES_CNT = 4633; // количество файлов в папке википедии
+    private static String srcFileWithSortedWords = "src\\com\\company\\resources\\sortedWordsWithRank";
+    // файл с рейтингованными английскими словами(вообще путь относительый - должен увидеться файл)
+
+    // Наладить этот метод указав путь к разархивированной википедии указав часть пути до нулей
+    private static String wikiPartOfPath = "C:\\wiki\\20140615-wiki-en_";
+
+    private static String iterateByWiki( Integer i ) {
+        if( i >= 1 && i < 10 ) {
+            return readLineByLine( wikiPartOfPath + "00000" + i + ".txt\\20140615-wiki-en_00000" + i +
+                                   ".txt" );
+        }
+        if( i >= 10 && i < 100 ) {
+            return readLineByLine( wikiPartOfPath + "0000" + i + ".txt\\20140615-wiki-en_0000" + i + "" +
+                                   ".txt" );
+        }
+        if( i >= 100 && i < 1000 ) {
+            return readLineByLine( wikiPartOfPath + "000" + i + ".txt\\20140615-wiki-en_000" + i + "" +
+                                   ".txt" );
+        }
+        if( i >= 1000 && i < 4633 ) {
+            return readLineByLine( wikiPartOfPath + "00" + i + ".txt\\20140615-wiki-en_00" + i + ".txt" );
+        } else {
+            return "";
+        }
+    }
+    // Все, остальное должно работать, вроде =)
 
     public static void main( String[] args ) throws IOException {
 
@@ -39,7 +68,6 @@ public class SentenceArranger {
 
         // Записываем эти предложения построчно в файл в виде [ слово  ^^^ ранк предложения  ###  пример предложения ]
 
-        // В папке /output должен появляться новый файлик каждые 1000 слов
         String fileForWrite = "src\\com\\company\\output\\engSentence_";
 
         String newFileForWrite;
@@ -56,22 +84,24 @@ public class SentenceArranger {
         try {
             for( Map.Entry<String, Integer> engWord : sortedMap.entrySet() ) {
                 wordNum++;
-                if( wordNum % 10 == 1 ) {
-                    newFileForWrite = fileForWrite + "_" + ( wordNum + 9 ) + ".txt";
+
+                // В папке /output должен появляться новый файлик каждые 1000 слов
+                if( wordNum % 1000 == 1 ) {
+                    newFileForWrite = fileForWrite + "_" + ( wordNum + 999 ) + ".txt";
                     bw = createNewFile( newFileForWrite );
                 }
                 if( wordNum >= 0 && null != bw ) { // для случая падения на каком-то слове
                     date = new Date();
-                    System.out.println( AnsiColor.BLUE +
+                    System.out.println( BLUE +
                                         new Timestamp( date.getTime() ) +
                                         "\n Word №: " + wordNum + " " +
                                         "--> " + engWord.getKey() +
-                                        AnsiColor.RESET );
+                                        RESET );
                     sentenceCounter = 0;
 
                     search:
                     {
-                        for( int i = 1; i < 4633; i++ ) {
+                        for( int i = 1; i < WIKI_FILES_CNT; i++ ) {
                             if( searchBreakerIfFew( sentenceCounter, i ) ) {
                                 break search;
                             }
@@ -113,12 +143,43 @@ public class SentenceArranger {
         }
     }
 
+    private static boolean isRightSentence( String sentence, Map.Entry<String, Integer> engWord ) {
+        //sentence = sentence.toLowerCase();
+        return sentence.length() > 20 &&
+               sentence.length() <= 128 &&
+               Character.isUpperCase( sentence.charAt( 0 ) ) &&
+               isAlphaWithPunctuation( sentence ) &&
+               !sentence.contains( "http" ) &&
+               !sentence.contains( "www" ) &&
+               ( ( sentence.contains( " " + engWord.getKey() + " " ) ) ||
+                 ( sentence.toLowerCase().indexOf( engWord.getKey() + " " ) == 0 ) ||
+                 ( sentence.endsWith( " " + engWord.getKey() ) )
+               );
+    }
+
+
+    private static String readLineByLine( String... filePath ) {
+        StringBuilder contentBuilder = new StringBuilder();
+        for( String f : filePath ) {
+            try (Stream<String> stream = Files.lines( Paths.get( f ), StandardCharsets.UTF_8 )) {
+                stream.forEach( s -> contentBuilder.append( s.trim() ).append( " " ) );
+            } catch( Exception e ) {
+                //
+            }
+        }
+        return contentBuilder.toString();
+    }
+
+    private static boolean isAlphaWithPunctuation( String name ) {
+        return name.matches( "[A-Za-z .,!\"'/$]*" );
+    }
+
 
     private static Map<String, Integer> readFileToMap() throws FileNotFoundException {
         Map<String, Integer> map = new HashMap<>();
         FileInputStream inputStream;
         Scanner sc;
-        inputStream = new FileInputStream( "src\\com\\company\\resources\\sortedWordsWithRank" );
+        inputStream = new FileInputStream( srcFileWithSortedWords );
         sc = new Scanner( inputStream, "UTF-8" );
         int counter = 0;
         while( sc.hasNextLine() ) {
@@ -127,7 +188,7 @@ public class SentenceArranger {
             map.put( a[ 0 ].trim(), Integer.parseInt( a[ 1 ].trim() ) );
             counter++;
         }
-        System.out.println( " Слова считаны. Всего " + counter );
+        System.out.println( GREEN + " Слова считаны. Всего " + counter + RESET );
         return map;
     }
 
@@ -145,7 +206,8 @@ public class SentenceArranger {
 
     private static boolean searchBreakerIfFew( int sentenceCounter, int i ) {
         if( i % 100 == 0 ) {
-            System.out.println( AnsiColor.YELLOW + " Считали уже " + i + " файлов wiki: " + sentenceCounter + " предложений найдено" + AnsiColor.RESET );
+            System.out.println( YELLOW + " Считали уже " + i + " файлов wiki: " + sentenceCounter + " предложений найдено" + RESET );
+            String searchBreked = RED + " Поиск прерван " + RESET;
             if( i == 300 && sentenceCounter == 0 ) {
                 System.out.println( searchBreked );
                 return true;
@@ -224,59 +286,5 @@ public class SentenceArranger {
 
     private static void searchedLog( int sentenceCounter ) {
         System.out.println( " Найдено всего " + sentenceCounter + " предложений " );
-    }
-
-    private static boolean isRightSentence( String sentence, Map.Entry<String, Integer> engWord ) {
-        //sentence = sentence.toLowerCase();
-        return sentence.length() > 20 &&
-               sentence.length() <= 128 &&
-               Character.isUpperCase( sentence.charAt( 0 ) ) &&
-               isAlphaWithPunctuation( sentence ) &&
-               !sentence.contains( "http" ) &&
-               !sentence.contains( "www" ) &&
-               ( ( sentence.contains( " " + engWord.getKey() + " " ) ) ||
-                 ( sentence.toLowerCase().indexOf( engWord.getKey() + " " ) == 0 ) ||
-                 ( sentence.endsWith( " " + engWord.getKey() ) )
-               );
-    }
-
-
-
-    private static String readLineByLine( String... filePath ) {
-        StringBuilder contentBuilder = new StringBuilder();
-        for( String f : filePath ) {
-            try (Stream<String> stream = Files.lines( Paths.get( f ), StandardCharsets.UTF_8 )) {
-                stream.forEach( s -> contentBuilder.append( s.trim() ).append( " " ) );
-            } catch( Exception e ) {
-                //
-            }
-        }
-        return contentBuilder.toString();
-    }
-
-    private static boolean isAlphaWithPunctuation( String name ) {
-        return name.matches( "[A-Za-z .,!\"'/$]*" );
-    }
-
-
-    // Наладить только этот метод -
-    private static String iterateByWiki( Integer i ) {
-        if( i >= 1 && i < 10 ) {
-            return readLineByLine( "C:\\wiki\\20140615-wiki-en_00000" + i + ".txt\\20140615-wiki-en_00000" + i +
-                                   ".txt" );
-        }
-        if( i >= 10 && i < 100 ) {
-            return readLineByLine( "C:\\wiki\\20140615-wiki-en_0000" + i + ".txt\\20140615-wiki-en_0000" + i + "" +
-                                   ".txt" );
-        }
-        if( i >= 100 && i < 1000 ) {
-            return readLineByLine( "C:\\wiki\\20140615-wiki-en_000" + i + ".txt\\20140615-wiki-en_000" + i + "" +
-                                   ".txt" );
-        }
-        if( i >= 1000 && i < 4633 ) {
-            return readLineByLine( "C:\\wiki\\20140615-wiki-en_00" + i + ".txt\\20140615-wiki-en_00" + i + ".txt" );
-        } else {
-            return "";
-        }
     }
 }
